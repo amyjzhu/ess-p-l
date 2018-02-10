b23b7b4ddd800c4ef27bde55b3cfec5f16efd9fd
amyjzhu <ajyzhu@gmail.com>
2018-02-10 14:19:24 -0800

    Add more optimization re representations

--- difftree.rkt
+++ difftree.rkt
@@ -17,7 +17,7 @@
 #;
 (define (fn-for-difftree dt)
   (cond [(and (string? dt)
-              (string=? "one")) ...]
+              (string=? "one" dt)) ...]
         [else (... dt
                    (fn-for-diff (diff-tree1 dt))
                    (fn-for-diff (diff-tree2 dt)))]))
@@ -112,10 +112,71 @@
 (check-expect (diff-sum (as-diff 19)) 19)
 
 (define (as-diff n)
-  (cond [(zero? n) (zero)]
-        [(< 0 n) (successor (as-diff (- n 1)))]
+  (cond [(= 1 n) "one"]
+        [(< 1 n) (successor (as-diff (- n 1)))]
         [else (predecessor (as-diff (+ n 1)))]))
 
+
+
+;; DiffTree -> Boolean
+;; produces true if shortest representation of difftree
+(check-expect (shortest-diff "one") true)
+(check-expect (shortest-diff ZERO) true)
+(check-expect (shortest-diff (make-diff THREE (make-diff "one" "one"))) false)
+(check-expect (shortest-diff (make-diff ZERO NEG-ONE)) false)
+
+(define (shortest-diff dt)
+  (local [(define (shortest-diff dt n)
+            (if (>= n 1)
+                (shortest-positive (diff-nodes dt) n)
+                (shortest-negative (diff-nodes dt) n)))]
+    (shortest-diff dt (diff-sum dt))))
+
+
+;; DiffTree -> Natural
+;; produces number of "one" difftrees in a difftree
+(check-expect (diff-nodes ZERO) 2)
+(check-expect (diff-nodes "one") 1)
+(check-expect (diff-nodes NEG-ONE) 3)
+(check-expect (diff-nodes (make-diff ZERO ZERO)) 4)
+
+(define (diff-nodes dt)
+  (fold-difftree 1 + dt))
+
+
+;; Natural -> Boolean
+;; produces true if number of nodes is shortest possible
+;; should be
+;; start with "one" and subtract -1 (i.e. (make-diff (make-diff "one" "one") "one")
+;; for each after. thus, (- n 1) * 3 + 1
+
+(check-expect (shortest-positive 1 1) true)
+(check-expect (shortest-positive 4 2) true)
+(check-expect (shortest-positive (+ 1 (* 5 3)) 6) true)
+(check-expect (shortest-positive 6 2) false)
+
+(define (shortest-positive nodes representation)
+  (= (+ (* (- representation 1) 3) 1) nodes))
+
+;; Natural -> Boolean
+;; produces true if number of
+(check-expect (shortest-negative 3 -1) true)
+(check-expect (shortest-negative 4 -2) true)
+(check-expect (shortest-negative 6 -2) false)
+;; each time, just add (diff dt "one") from base negative one
+              
+(define (shortest-negative nodes representation)
+  (= nodes (+ 3 (* (- -1 representation) 1))))
+
+
+
+;; abstract fold
+(define (fold-difftree b1 c1 dt)
+  (cond [(and (string? dt)
+              (string=? "one" dt)) b1]
+        [else (c1 
+                   (fold-difftree b1 c1 (diff-tree1 dt))
+                   (fold-difftree b1 c1 (diff-tree2 dt)))]))
 
 
 ;; DiffTree -> DiffTree
